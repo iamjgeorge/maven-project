65ee4e2069606d7ee4c469d2779a8385d53b5d9b
Jacob George <amjgeorge@yahoo.com>
2017-11-14 21:39:22 +1100

    fixes for check stly warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,9 +12,9 @@ public class Greeter {
 
   }
 
-  /*
-  @param someone get someone
-  @return return something
+  /**
+  * @param someone get someone
+  * @return return something
   */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
